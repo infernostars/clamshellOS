5c35eaca5ef19c55a1fa3fccd7f232386d745657
infernostars <blazem7708.2@gmail.com>
2023-05-20 02:11:19 -0600

    broken tests rn but thats fine

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(the_operator::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+use the_operator::println;
 use core::panic::PanicInfo;
-
-mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    the_operator::test_panic_handler(info)
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
